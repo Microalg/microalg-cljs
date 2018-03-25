79f06bac079935291f1160130bfd241e416c7b54
Christophe Gragnic <cgragnic@netc.fr>
2018-03-25 12:16:14 +0200

    Better parse: handle single atoms

--- src/microalg/parser.cljs
+++ src/microalg/parser.cljs
@@ -5,11 +5,11 @@
 ; https://stackoverflow.com/questions/18187249/how-do-we-define-a-grammar-for-clojure-code-using-instaparse
 (defparser grammar
   "<src> = atom | operation
-   <atom> = num / str / sym
+   atom = num / str / sym
    num = #'[0-9]+'
    str = '\"' '\"'
    sym = #'[a-zA-Z0-9_=+-/*~|{}^!?#$%&\\'`]*'
-   <operation> = <'('> wspace* src (wspace+ src)* wspace* <')'>
+   operation = <'('> wspace* src (wspace+ src)* wspace* <')'>
    <wspace>  = <#'\\s'>")
 
 (defn read-num
@@ -19,5 +19,6 @@
 (defn parser
   [src]
   (insta/transform
-    {:num read-num :str identity :sym symbol}
-    (grammar src)))
+    {:atom identity :operation list
+     :num read-num :str identity :sym symbol}
+    (first (grammar src))))
--- test/microalg/parser_test.cljs
+++ test/microalg/parser_test.cljs
@@ -4,5 +4,7 @@
 
 (deftest sexpr-test
   (are [expected actual] (= expected actual)
+       1 (parser "1")
+       'x (parser "x")
        '(+ 2 2) (parser "(+ 2 2)")
        ))
