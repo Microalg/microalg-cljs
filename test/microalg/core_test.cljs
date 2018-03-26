1013a5d223d8854b5c799a685df91e2cff1b90ad
Christophe Gragnic <cgragnic@netc.fr>
2018-03-26 03:52:49 +0200

    core.test: do not involve the parser

--- test/microalg/core_test.cljs
+++ test/microalg/core_test.cljs
@@ -1,8 +1,8 @@
 (ns microalg.core-test
     (:require [cljs.test :refer-macros [deftest are]]
-              [microalg.core :refer (evaluate-str)]))
+              [microalg.core :refer (evaluate env-global)]))
 
 (deftest arithmetics-test
     (are [expected actual] (= expected actual)
-         3 (evaluate-str "(+ 1 2)")
-         6 (evaluate-str "(+ 1 (+ 2 3))")))
+         3 (evaluate '(+ 1 2) env-global)
+         6 (evaluate '(+ 1 (+ 2 3)) env-global)))
