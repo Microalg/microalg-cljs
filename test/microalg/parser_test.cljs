4f4ac17c569203749557e65036509a0394f94cc9
Christophe Gragnic <cgragnic@netc.fr>
2018-03-27 12:49:28 +0200

    tests: reformat parse error expected results

--- test/microalg/parser_test.cljs
+++ test/microalg/parser_test.cljs
@@ -25,23 +25,30 @@
 (deftest errors-test
   (are [actual expected] (= actual expected)
        (parser "1 2")
-       [:parse-error {:line 1 :column 1
-        :info ["opening parenthesis" "number" "string" "symbol"]}]
+       [:parse-error
+        {:line 1 :column 1
+         :info ["opening parenthesis" "number" "string" "symbol"]}]
        (parser "(")
-       [:parse-error {:line 1 :column 2
-        :info ["opening parenthesis" "number" "whitespace" "string" "symbol"]}]
+       [:parse-error
+        {:line 1 :column 2
+         :info ["opening parenthesis" "number" "whitespace" "string" "symbol"]}]
        (parser ")")
-       [:parse-error {:line 1 :column 1
-        :info ["opening parenthesis" "number" "string" "symbol"]}]
+       [:parse-error
+        {:line 1 :column 1
+         :info ["opening parenthesis" "number" "string" "symbol"]}]
        (parser "()")
-       [:parse-error {:line 1 :column 2
-        :info ["opening parenthesis" "number" "whitespace" "string" "symbol"]}]
+       [:parse-error
+        {:line 1 :column 2
+         :info ["opening parenthesis" "number" "whitespace" "string" "symbol"]}]
        (parser "(+")
-       [:parse-error {:line 1 :column 3
-        :info ["closing parenthesis" "whitespace"]}]
+       [:parse-error
+        {:line 1 :column 3
+         :info ["closing parenthesis" "whitespace"]}]
        (parser "(+ 2")
-       [:parse-error {:line 1 :column 5
-        :info ["closing parenthesis" "whitespace"]}]
+       [:parse-error
+        {:line 1 :column 5
+         :info ["closing parenthesis" "whitespace"]}]
        (parser "(+ 2 2")
-       [:parse-error {:line 1 :column 7
+       [:parse-error
+        {:line 1 :column 7
         :info ["closing parenthesis" "whitespace"]}]))
