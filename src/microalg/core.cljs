256c431e0165c9138cd44a8511164584ef9fce65
Christophe Gragnic <cgragnic@netc.fr>
2018-04-09 13:40:38 +0200

    Naive integration of the interpreter in jQ term

--- src/microalg/core.cljs
+++ src/microalg/core.cljs
@@ -1,7 +1,16 @@
 (ns microalg.core
     (:require [jquery]
-              [jquery.terminal]))
+              [jquery.terminal]
+              [microalg.interp :as interp]))
 
 (def app-div (js/$ "#app"))
+(def options {:clear  false
+              :exit   false
+              :prompt ":> "})
+
 (.html app-div "")
-(def term (.terminal app-div))
+(def term (.terminal app-div
+                     #(clj->js (interp/evaluate-str % interp/env-global))
+                     (clj->js options)))
+(.clear term)
+(.echo term "MicroAlg")
